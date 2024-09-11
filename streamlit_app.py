85ee6f378d0cd1ffcc6c274f439e00bf5b6fddba
awaikar-syr <awaikar@syr.edu>
2024-09-11 23:30:49 +0000

    first commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,22 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.set_page_config(page_title="HW Manager")
+
+st.title("HW Manager")
+
+# Add a menu to navigate between pages
+pages = {
+    "HW1": HWs.py,
+    "HW2": HWs.py,
+}
+
+# Sidebar for navigation
+st.sidebar.title("HW Manager")
+selection = st.sidebar.radio("Go to", list(pages.keys()))
+
+# Run selected page
+page = pages[selection]
+page.app()
+
+
+
