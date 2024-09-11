cb60acf2f056b8739d3d3d1732bfa1396f444bb4
awaikar-syr <awaikar@syr.edu>
2024-09-11 23:55:12 +0000

    8

--- HW2.py
+++ HW2.py
@@ -1,3 +1,47 @@
 import streamlit as st
+from openai import OpenAI
 
-#st.set_page_config(page_title="HW 2")
+st.title("This is HW 2")
+
+
+openai_api_key = st.secrets["OPENAI_KEY"] 
+
+if not openai_api_key:
+    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+else:
+
+    # Create an OpenAI client.
+    client = OpenAI(api_key=openai_api_key)
+
+    # Let the user upload a file via ⁠ st.file_uploader ⁠.
+    uploaded_file = st.file_uploader(
+        "Upload a document (.txt or .md)", type=("txt", "md")
+    )
+
+    # Ask the user for a question via ⁠ st.text_area ⁠.
+    question = st.text_area(
+        "Now ask a question about the document!",
+        placeholder="Can you give me a short summary?",
+        disabled=not uploaded_file,
+    )
+
+    if uploaded_file and question:
+
+        # Process the uploaded file and question.
+        document = uploaded_file.read().decode()
+        messages = [
+            {
+                "role": "user",
+                "content": f"Here's a document: {document} \n\n---\n\n {question}",
+            }
+        ]
+
+        # Generate an answer using the OpenAI API.
+        stream = client.chat.completions.create(
+            model="gpt-4o-mini",
+            messages=messages,
+            stream=True,
+        )
+
+        # Stream the response to the app using ⁠ st.write_stream ⁠.
+        st.write_stream(stream)
