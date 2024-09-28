292a3fa963f0f789f3cc36944207c6d865b1a356
awaikar-syr <awaikar@syr.edu>
2024-09-28 04:44:07 +0000

    HW4 Final Commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,8 @@ st.title("Ankit's Homework Manager")
 
 hw1_page = st.Page("HW1.py", title="Homework 1")
 hw2_page = st.Page("HW2.py", title="Homework 2")
-hw3_upd = st.Page("HW3_upd.py", title="Homework 3", default=True)
-hw4_page = st.Page("HW4.py", title="Homework 4")
+hw3_upd = st.Page("HW3_upd.py", title="Homework 3")
+hw4_page = st.Page("HW4.py", title="Homework 4", default=True)
 
 pg = st.navigation([hw1_page, hw2_page, hw3_upd, hw4_page])
 
