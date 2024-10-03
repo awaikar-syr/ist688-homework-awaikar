7459e59ad2c8b1064dc2a4fc9d57f0e7556f1438
awaikar-syr <awaikar@syr.edu>
2024-10-03 00:37:54 +0000

    rename hw5 in app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ hw1_page = st.Page("HW1.py", title="Homework 1")
 hw2_page = st.Page("HW2.py", title="Homework 2")
 hw3_upd = st.Page("HW3_upd.py", title="Homework 3")
 hw4_page = st.Page("HW4.py", title="Homework 4")
-hw5_page = st.Page("HW5-upd.py", title="Homework 5", default=True)
+hw5_page = st.Page("HW5_upd.py", title="Homework 5", default=True)
 
 
 pg = st.navigation([hw1_page, hw2_page, hw3_upd, hw4_page, hw5_page])
